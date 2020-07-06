5ee19463a7e6eb619b0f53110d9cbed71e7cb96a
Laura THAY <laura.thay@ipag.fr>
2020-07-06 13:17:05 +0200

    sidebar done et index a completer
    pareil pour front-page quelques zones noir

---
+++ wp-content/themes/laura/sidebar.php
@@ -0,0 +1,11 @@
+<aside class="sidebar">
+  <?php
+    if(is_active_sidebar('widgets-sidebar')) :
+      dynamic_sidebar('widgets-sidebar');
+    endif;
+  ?>
+  <!-- <div class="widget">
+    <h3 class="widget-title">Zone de widgets</h3>
+    <p>Ajout dynamique des titres et contenus des widgets.</p>
+  </div> -->
+</aside>
