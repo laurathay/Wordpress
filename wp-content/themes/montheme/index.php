93a1893bdfaa60c645f523ee3d2ff94ea0bddbc1
Laura THAY <laura.thay@ipag.fr>
2022-07-26 21:57:23 +0200

    [add] boucle article

--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,4 +1,22 @@
 <?php get_header() ?>
 <!-- elle prend en parametre un separateur et recupere le titre modifier -->
 Hello world : <?php wp_title() ?> 
+
+<?php if (have_posts()): ?>
+    <ul>
+        <!-- pour avoir le post qui change dans le dashboard WP -->
+    <?php while(have_posts()): the_post(); ?> 
+        <li>
+            <a href="<?php the_permalink() ?>">
+            <?php the_title() ?> 
+            </a>
+            -
+            <?php the_author() ?>
+        </li>
+    <?php endwhile ?>
+    </ul>
+<?php else: ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
+
 <?php get_footer() ?>
